4e660efd0e4b6ad05db4116a23ff93f35f9c9bc7
Teddy van Jerry <me@teddy-van-jerry.org>
2022-07-20 20:45:45 +0800

    OpenBLAS Copy Files

--- set_openblas.ps1
+++ set_openblas.ps1
@@ -1,5 +1,5 @@
 mkdir ./mingw64/dll
-Copy-Item -Path ./openblas/include -Destination ./mingw64/x86_64-w64-mingw32/include -Recurse
+Copy-Item -Path ./openblas/include/* -Destination ./mingw64/x86_64-w64-mingw32/include -Recurse
 Copy-Item -Path ./openblas/bin/libopenblas.dll -Destination ./mingw64/dll/libopenblas.dll
-Copy-Item -Path ./openblas/lib -Destination ./mingw64/x86_64-w64-mingw32/lib -Recurse
+Copy-Item -Path ./openblas/lib/* -Destination ./mingw64/x86_64-w64-mingw32/lib -Recurse
 Invoke-WebRequest https://raw.githubusercontent.com/xianyi/OpenBLAS/develop/LICENSE -o./mingw64/licenses/OpenBLAS.txt
